680b4d5f31920a6f9cb5db651b1a9328217c3495
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2024-04-26 15:11:52 +0600

    Module 11.5 Done

---
+++ COURSE-1-Intro2ProgLanguage/WEEK-3/Module 11.5/V_Frequency_Array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+    int n, m;
+    scanf("%d%d",&n,&m);
+    int ar[n];
+    int cnt[100001] = {0};
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&ar[i]);
+        cnt[ar[i]]++;
+    }
+
+
+    for(int i=1; i<=m; i++)
+    {
+        printf("%d\n",cnt[i]);
+    }
+    
+    return 0;
+}
