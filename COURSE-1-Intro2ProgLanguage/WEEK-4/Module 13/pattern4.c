ff4240eabc8749b7104942b181cf1ed96068c935
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2024-04-30 21:12:25 +0600

    Module 13 Complete

--- COURSE-1-Intro2ProgLanguage/WEEK-4/Module 13/pattern4.c
+++ COURSE-1-Intro2ProgLanguage/WEEK-4/Module 13/pattern4.c
@@ -1,21 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
+    // daimond
     int n;
-    scanf("%d",&n);
-    printf("%d\n",n);
+    scanf("%d", &n);
+    int space = n - 1;
+    int star = 1;
 
-    int k=1;
-    for(int i=1; i<=n; i++)
+    for (int i = 1; i <= 2 * n; i++)
     {
-        for(int j=1; j<=k; j++)
+        for (int j = 1; j <= space; j++)
         {
-            // printf("Hi honey\n");
-            printf("%d",j);
+            printf(" ");
         }
+        for (int j = 1; j <= star; j++)
+        {
+            printf("*");
+        }
+
+        if (i < n)
+        {
+            space--;
+            star += 2;
+        }
+        else
+        {
+            space++;
+            star -= 2;
+        }
+
         printf("\n");
-        k++;
     }
-    
+
+
     return 0;
 }
