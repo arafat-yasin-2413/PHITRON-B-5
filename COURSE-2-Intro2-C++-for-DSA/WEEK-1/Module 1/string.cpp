6ba2e07a89bdcc16ca6af004e6a8ba2e77f0474b
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2024-05-24 07:17:33 +0600

    Module 1 Basic C++

--- COURSE-2-Intro2-C++-for-DSA/WEEK-1/Module 1/string.cpp
+++ COURSE-2-Intro2-C++-for-DSA/WEEK-1/Module 1/string.cpp
@@ -1,3 +1,5 @@
+
+/*
 #include <iostream>
 #include <bits/stdc++.h>
 #include <iomanip>
@@ -21,6 +23,32 @@ int main()
     cout << x << endl;
 
     cout<< x << " "<<ar<<endl;
+
+    return 0;
+}
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()  
+{
+    char ar[100];
+    // character array input nite loop lage na
+    // cin>>ar;
+    // fgets(ar,100,stdin);
+
+
+    int x;
+    cin>>x;
+    getchar(); 
+
+    cin.getline(ar,100);
+
+    cout<<x<<endl;
+    cout<<ar<<endl;
+    // cout<<strlen(ar)<<endl;
 
     return 0;
 }
