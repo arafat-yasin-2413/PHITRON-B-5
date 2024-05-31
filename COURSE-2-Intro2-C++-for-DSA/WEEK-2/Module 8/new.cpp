07c5eb037950dfa5e506b87ddad20b0f0dd26483
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2024-05-31 22:40:34 +0600

    doing drafts for Find Jessica

--- COURSE-2-Intro2-C++-for-DSA/WEEK-2/Module 8/new.cpp
+++ COURSE-2-Intro2-C++-for-DSA/WEEK-2/Module 8/new.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -31,6 +32,7 @@ int main()
     else
     {
         int temp_pos = s.find("Jessica");
+        cout << "temp pos = " << temp_pos << endl;
         if (temp_pos != -1)
         {
             if (s[temp_pos - 1] == ' ' && s[s.size() - 1] == '\0') // Ratul Rahat Jessica
@@ -56,7 +58,7 @@ int main()
                 flag = 0;
             }
         }
-        else if(temp_pos == -1)
+        else if (temp_pos == -1)
         {
             flag = 0;
         }
@@ -73,3 +75,54 @@ int main()
 
     return 0;
 }
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char s[1001];
+    // getline(cin,s);
+    cin.getline(s,1001);
+
+    cout << s << endl;
+
+    // int temp_pos = s.find("Jessica");
+    // cout << "temp pos = " << temp_pos << endl;
+
+    return 0;
+}
+
+/*
+hints
+প্রথমে N কে declare করে ইনপুট নিবেন। এরপর N size এর array of object বানাবেন ( যেইটির জন্য একটি class বানাবেন যেইখানে nm, cls, s, id, math_marks, eng_marks এই variable গুলোকে declare করবেন )।
+ এরপর N বার লুপ চালিয়ে student এর details গুলো ইনপুট নিবেন, array of object এর মধ্যে।
+
+এরপর array of object কে build-in sort function দিয়ে sort করবেন,সেইখানে ৩য় argument এ Compare Predicate (cmp) ব্যাবহার করবেন ।
+
+এরপর bool type cmp function লিখবেন যেইখানে 2 টি parameter থাকবে ,সেই 2 টি parameter এ ২টি object পাবেন।
+
+সেই object ২টির eng_marks conditions দিয়ে compare করবেন এবং সেই অনুযায়ী true অথবা false return করবেন।
+
+যদি object ২টির eng_marks মিলে যায় তাহলে object ২টির math_marks উপর ভিত্তি করে true অথবা false return করবেন।
+
+
+test 4
+input
+
+Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Rahat Rifat Sakib Asif Sifat Munna Jessica
+
+
+output: Yes
+
+
+test 5
+
+input
+JessiCa JesSica jessica JESSICA JessicaJessica JEssica JessicA JessIca JessiCa Jessicajessica Jessiica Jessicar JJessica
+
+output: no
+
+*/
