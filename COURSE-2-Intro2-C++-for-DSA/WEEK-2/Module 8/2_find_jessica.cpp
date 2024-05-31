72ff4f318cdc7fdf2206e14aee0df39f41ff15f2
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2024-05-31 16:48:59 +0600

    WA on case 8,10,17 problem - Find Jessica

--- COURSE-2-Intro2-C++-for-DSA/WEEK-2/Module 8/2_find_jessica.cpp
+++ COURSE-2-Intro2-C++-for-DSA/WEEK-2/Module 8/2_find_jessica.cpp
@@ -6,75 +6,56 @@ int main()
     string s;
     getline(cin, s);
 
-    // cout << s << endl;
-
-    int position = s.find("Jessica");
-    // cout << position << endl;
+    // cout<<s<<endl;
 
     int flag = 0;
-    if (position != -1)
+
+    int position = s.find("Jessica");
+    int idx_of_last_a = position + 6;
+    // cout << "Position = " << position << endl;
+
+    // dui pashei space
+    if (s[position - 1] == ' ' && s[position + 7] == ' ')
     {
-        // jodi shurutei Jessica thake
-        if (s[0] == 'J') // Jessica Rifat
-        {
-            if (s.size() > 7) // Jessica Rifat
-            {
-                if (s[position + 7] == ' ')
-                {
-                    flag = 1;
-                    // cout<<"Jessica paisi"<<endl;
-                    // cout << "eikhane aschi 1" << endl;
-                }
-            }
-            else if (s.size() == 7) // Jessica
-            {
-                if (s == "Jessica")
-                {
-                    flag = 1;
-                    // cout<<"Paisi"<<endl;
-                    // cout << "eikhane aschi 2" << endl;
-                }
-            }
-        }
+        // cout << "dui pashei space paisi" << endl;
+        flag = 1;
+    }
 
-        // jodi ekdom seshe Jessica thake
-        else if (s[position + 6] == 'a')
-        {
-            // char x3 = s[position + 6];
-            if (position + 6 == s.size() - 1)
-            {
-                flag = 1;
-                // cout << "eikhane aschi 3" << endl;
-            }
-        }
+    // shudhu baame space and Jessica diye sesh
+    else if (s[position - 1] == ' ' && s[idx_of_last_a + 1] == '\0')
+    {
+        // cout << "baame space r daane null char paisi r Jessica diyei sesh" << endl;
+        // cout << "Jessica er last a = " << idx_of_last_a << endl;
+        // cout << "s er length = " << s.size() << endl;
+        flag = 1;
+    }
 
-        // Jessicar ashe pashe jodi kichu thake
-        else if (s[position - 1] == ' ' && s[position + 7] == ' ')
-        {
-            flag = 1;
-            // cout << "eikhane aschi 4" << endl;
-        }
+    // shudhu daane space and Jessica diyei shuru
+    else if (s[0] == 'J' && s[position + 7] == ' ')
+    {
+        // cout << "Shudhu daane space paisi. Jessica diye shuru hoise" << endl;
+        flag = 1;
+    }
 
-        else if (s[position - 1] != ' ' && s[position + 7] != ' ')
-        {
-            char x1 = s[position - 1];
-            char x2 = s[position + 7];
-            cout << "x1 = " << x1 << endl;
-            cout << "x2 = " << x2 << endl;
-            // cout << "eikhane aschi 5" << endl;
-            if ((x1 >= 'a' && x1 <= 'z') or (x1 >= 'A' && x2 <= 'Z'))
-            {
-                flag = 0;
-                // cout << "eikhane aschi 6" << endl;
-            }
-        }
+    // dui pashei no space
+    else if (s.size() == 7)
+    {
+        // cout << "Only Jessica paisi" << endl;
+        flag = 1;
+    }
+
+    else
+    {
+        // cout << "kichui pai nai" << endl;
+        flag = 0;
     }
 
     if (flag == 1)
     {
         cout << "YES" << endl;
     }
-    else if (flag == 0)
+
+    else
     {
         cout << "NO" << endl;
     }
