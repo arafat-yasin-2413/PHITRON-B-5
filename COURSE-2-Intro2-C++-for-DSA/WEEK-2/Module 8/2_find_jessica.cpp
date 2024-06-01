c1c85128b75a4c2bb80bec55ce6f73b2b035ee0e
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2024-06-01 14:00:01 +0600

    Find Jessica Done

--- COURSE-2-Intro2-C++-for-DSA/WEEK-2/Module 8/2_find_jessica.cpp
+++ COURSE-2-Intro2-C++-for-DSA/WEEK-2/Module 8/2_find_jessica.cpp
@@ -6,55 +6,24 @@ int main()
     string s;
     getline(cin, s);
 
-    // cout<<s<<endl;
+    // cout << s << endl;
+    stringstream ss(s);
 
+    string x = "Jessica";
+    string word;
     int flag = 0;
-
-    int position = s.find("Jessica");
-    int idx_of_last_a = position + 6;
-    // cout << "Position = " << position << endl;
-
-    // dui pashei space
-    if (s[position - 1] == ' ' && s[position + 7] == ' ')
+    while (ss >> word)
     {
-        // cout << "dui pashei space paisi" << endl;
-        flag = 1;
+        // cout << word << endl;
+        if (word == x)
+        {
+            flag = 1;
+        }
     }
-
-    // shudhu baame space and Jessica diye sesh
-    else if (s[position - 1] == ' ' && s[idx_of_last_a + 1] == '\0')
-    {
-        // cout << "baame space r daane null char paisi r Jessica diyei sesh" << endl;
-        // cout << "Jessica er last a = " << idx_of_last_a << endl;
-        // cout << "s er length = " << s.size() << endl;
-        flag = 1;
-    }
-
-    // shudhu daane space and Jessica diyei shuru
-    else if (s[0] == 'J' && s[position + 7] == ' ')
-    {
-        // cout << "Shudhu daane space paisi. Jessica diye shuru hoise" << endl;
-        flag = 1;
-    }
-
-    // dui pashei no space
-    else if (s.size() == 7)
-    {
-        // cout << "Only Jessica paisi" << endl;
-        flag = 1;
-    }
-
-    else
-    {
-        // cout << "kichui pai nai" << endl;
-        flag = 0;
-    }
-
     if (flag == 1)
     {
         cout << "YES" << endl;
     }
-
     else
     {
         cout << "NO" << endl;
@@ -62,3 +31,4 @@ int main()
 
     return 0;
 }
+
--- COURSE-2-Intro2-C++-for-DSA/WEEK-2/Module 8/5_sort_it_again.cpp
+++ COURSE-2-Intro2-C++-for-DSA/WEEK-2/Module 8/5_sort_it_again.cpp
@@ -1,32 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Student
+{
+public:
+    string name;
+    int cls;
+    char section;
+    int id;
+    int math_marks;
+    int eng_marks;
+};
+
+bool cmp(Student a, Student b)
+{
+   
+    if(a.eng_marks == b.eng_marks)
+    {
+        if (a.math_marks > b.math_marks)
+            return true;
+        else
+            return false;
+    }    
+
+    if(a.math_marks == b.math_marks)
+    {
+        if(a.id < b.id)
+            return true;
+        else
+            return false;
+    }
+
+    if(a.eng_marks > b.eng_marks)
+        return true;
+    else
+        return false;
+
+
+    // if (a.eng_marks != b.eng_marks)
+    // {
+    //     return a.eng_marks > b.eng_marks;
+    // }
+    // else if (a.eng_marks == b.eng_marks)
+    // {
+    //     return a.math_marks > b.math_marks;
+    // }
+    // else if(a.math_marks == b.math_marks)
+    // {
+    //     return a.id < b.id;
+    // }
+}
 int main()
 {
-    string s;
-    getline(cin, s);
+    int n;
+    cin >> n;
 
-    // cout << s << endl;
-    stringstream ss(s);
+    Student ar[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ar[i].name >> ar[i].cls >> ar[i].section >> ar[i].id >> ar[i].math_marks >> ar[i].eng_marks;
+    }
 
-    string x = "Jessica";
-    string word;
-    int flag = 0;
-    while (ss >> word)
+    sort(ar, ar + n, cmp);
+
+    for (int i = 0; i < n; i++)
     {
-        // cout << word << endl;
-        if (word == x)
-        {
-            flag = 1;
-        }
-    }
-    if (flag == 1)
-    {
-        cout << "YES" << endl;
-    }
-    else
-    {
-        cout << "NO" << endl;
+        cout << ar[i].name << " " << ar[i].cls << " " << ar[i].section << " " << ar[i].id << " " << ar[i].math_marks << " " << ar[i].eng_marks << endl;
     }
 
     return 0;
