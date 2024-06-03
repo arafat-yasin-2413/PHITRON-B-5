062ae9832b65cd8c77c47e57e71351f8f8fca420
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2024-06-04 00:37:21 +0600

    Indentation and Explanation added for sort it again

--- COURSE-2-Intro2-C++-for-DSA/WEEK-2/Module 8/5_sort_it_again.cpp
+++ COURSE-2-Intro2-C++-for-DSA/WEEK-2/Module 8/5_sort_it_again.cpp
@@ -27,6 +27,49 @@ a id choto kina , chto hoile
 
 bool cmp(Student a, Student b)
 {
+    // eng boro kina
+    if(a.eng_marks > b.eng_marks)
+    {
+        return true;
+    }
+    // eng choto kina
+    else if(a.eng_marks < b.eng_marks)
+    {
+        return false;
+    }
+    // else (eng boro o na, eng choto o na , maane eng soman)
+    else
+    {
+        // ekhon ager moto math boro kina check korbo
+        if(a.math_marks > b.math_marks)
+        {
+            return true;
+        }
+        // math choto kina check korbo
+        else if(a.math_marks < b.math_marks)
+        {
+            return false;
+        }
+        // math soman kina check korbo
+        else if(a.math_marks == b.math_marks)
+        {
+            // ekhon id er vitti te kaj korbo 
+            if(a.id < b.id)
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+        
+        
+    }
+
+
+
+/*
     if (a.eng_marks > b.eng_marks)
     {
         return true;
@@ -58,6 +101,8 @@ bool cmp(Student a, Student b)
             return false;
         }
     }
+
+ */   
 }
 int main()
 {
